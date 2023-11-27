f90d1c91878f022102e407edd648c3dea28857df
popska <popska@tuta.io>
2023-11-26 20:27:00 -0700

    First implementation of distributed clustering

--- src/distributed_cpu_k_clustering.c
+++ src/distributed_cpu_k_clustering.c
@@ -15,17 +15,6 @@ void k_means_clustering(Point* points, int points_size, int epochs, int k) {
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	/// Initialize centroids
-	Point centroids[k];
-	if (my_rank == 0)
-	{
-		//srand(time(0));
-		srand(42);
-		for (int i = 0; i < k; ++i) {
-			centroids[i] = points[rand() % points_size];
-		}
-	}
-
 	/// distribute sections of the array
 	int sub_points_size = points_size / comm_sz;
 	if (points_size % comm_sz != 0) sub_points_size++;
@@ -53,7 +42,107 @@ void k_means_clustering(Point* points, int points_size, int epochs, int k) {
 	MPI_Datatype mpi_point_type = create_point_datatype();
 
 	MPI_Scatterv(points, send_counts, displs, mpi_point_type,
-		     sub_points, send_counts[my_rank], mpi_point_type, 0, MPI_COMM_WORLD);
+		     sub_points, send_counts[my_rank], mpi_point_type,
+		     0, MPI_COMM_WORLD);
+
+	/// Initialize centroids
+	Point centroids[k];
+	if (my_rank == 0)
+	{
+		//srand(time(0));
+		srand(42);
+		for (int i = 0; i < k; ++i) {
+			centroids[i] = points[rand() % points_size];
+		}
+	}
+
+	/// do the k clustering
+	for (int i = 0; i < epochs; ++i) {
+		// make sure the centroids are the same (take from rank 0)
+		MPI_Barrier(MPI_COMM_WORLD);
+		MPI_Bcast(centroids, k, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+		// For each centroid, compute distance from centroid to each point
+		// and update point's cluster if necessary
+		for (int cluster_id = 0; cluster_id < k; ++cluster_id){
+			Point c = centroids[cluster_id];
+
+			for (int j = 0; j < sub_points_size; ++j) {
+				Point* p = &sub_points[j];
+				double dist = point_distance(c, *p);
+				if (dist < p->min_dist) {
+					p->min_dist = dist;
+					p->cluster = cluster_id;
+				}
+			}
+		}
+
+		// compute the local sums
+		// Create vectors to keep track of data needed to compute means
+		int sub_n_points[k];
+		double sub_sum_x[k];
+		double sub_sum_y[k];
+		double sub_sum_z[k];
+		for (int j = 0; j < k; ++j) {
+			sub_n_points[j] = 0;
+			sub_sum_x[j] = 0.0;
+			sub_sum_y[j] = 0.0;
+			sub_sum_z[j] = 0.0;
+		}
+
+		// Iterate over points to append data to centroids
+		for (int j = 0; j < sub_points_size; j++) {
+			Point* p = &sub_points[j];
+			sub_n_points[p->cluster] += 1;
+			sub_sum_x[p->cluster] += p->x;
+			sub_sum_y[p->cluster] += p->y;
+			sub_sum_z[p->cluster] += p->z;
+
+			// reset distance
+			p->min_dist = DBL_MAX;
+		}
+
+		// reduce the sums and then let rank 0 update the centroids
+		int n_points[k];
+		double sum_x[k];
+		double sum_y[k];
+		double sum_z[k];
+		// TODO: is this necessary?
+		if (my_rank == 0) {
+			for (int j = 0; j < k; ++j) {
+				n_points[j] = 0;
+				sum_x[j] = 0.0;
+				sum_y[j] = 0.0;
+				sum_z[j] = 0.0;
+			}
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		MPI_Reduce(sub_n_points, n_points, k, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sub_sum_x, sum_x, k, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sub_sum_y, sum_y, k, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sub_sum_z, sum_z, k, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+		if (my_rank == 0) {
+			for (int cluster_id = 0; cluster_id < k; cluster_id++) {
+				Point* c = &centroids[cluster_id];
+				if (n_points[cluster_id] != 0) {
+					c->x = sum_x[cluster_id] / n_points[cluster_id];
+					c->y = sum_y[cluster_id] / n_points[cluster_id];
+					c->z = sum_z[cluster_id] / n_points[cluster_id];
+				}
+			}
+		}
+	}
+
+	/// send all the results back to rank 0 and update
+	MPI_Gatherv(sub_points, send_counts[my_rank], mpi_point_type,
+		    points, send_counts, displs, mpi_point_type,
+		    0, MPI_COMM_WORLD);
+
+	free(displs);
+	free(send_counts);
+	free(sub_points);
 
 	MPI_Finalize();
 }
