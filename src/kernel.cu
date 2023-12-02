f9878a9b2fc37874b8bf60c2ab9c18881d9cfa5d
Marcus Quincy <u1120377@notchpeak2.int.chpc.utah.edu>
2023-12-02 15:56:09 -0700

    Change GPU distance calculation to only allocate memory once

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,3 +1,5 @@
+// -*- mode: c -*-
+
 #include <cstdlib>
 #include <stdio.h>
 #include <float.h>
@@ -5,87 +7,84 @@
 #include "point.h"
 
 //Testing for CUDA errors
-inline cudaError_t checkCuda(cudaError_t result) {
-if (result != cudaSuccess) {
-fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-}
-return result;
+#define checkCuda(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
 }
 
 //Clustering Calculation
 __global__ 
-void my_Kernel_1(Point* points_d, int points_size, Point* c_d, int cluster_id) {
-    
+void distances_k(Point* points_d, int points_size, Point* centroids_d, int k) {
     //Get the index for the current point to work with
-    int Col = threadIdx.x + blockIdx.x * blockDim.x;
-    int Row = threadIdx.y + blockIdx.y * blockDim.y;
-    int index = Col + (Row * blockDim.x *gridDim.x); 
-    
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+    /*
+      Potential improvement would be to not interate cluster_id -> k and instead have a separate thread
+      per cluster_id. Not sure if this would yield any performance gains though. It also creates race
+      conditions that have to be dealt with.
+     */
+
     if(index < points_size) {
-        Point p = points_d[index];
-        double dist = ((c_d->x - p.x) * (c_d->x - p.x) + (c_d->y - p.y) * (c_d->y - p.y) + (c_d->z - p.z) * (c_d->z - p.z));// point_distance(c, *p);
-        if (dist < p.min_dist) {
-            p.min_dist = dist;
-            p.cluster = cluster_id;
+        //printf("idx %d\n", index);
+        Point* p = &points_d[index];
+        for (int cluster_id = 0; cluster_id < k; cluster_id++) {
+            Point c = centroids_d[cluster_id];
+            double dist = ((c.x - p->x) * (c.x - p->x) + (c.y - p->y) * (c.y - p->y) + (c.z - p->z) * (c.z - p->z));// point_distance(c, *p);
+            if (dist < p->min_dist) {
+                p->min_dist = dist;
+                p->cluster = cluster_id;
+            }
         }
-        
-        points_d[index] = p;
     }
 }
 
-__global__
-void my_Kernel_2(double max, Point* points_d, int points_size, int* n_points_d, double* sum_x_d, double* sum_y_d, double* sum_z_d) {
-    //Get the index of the point to work with
-    int Col = threadIdx.x + blockIdx.x * blockDim.x;
-    int Row = threadIdx.y + blockIdx.y * blockDim.y;
-    int index = Col + (Row * blockDim.x * gridDim.x); 
-    
-    if(index < points_size) {
-        Point p = points_d[index];
-        n_points_d[p.cluster] += 1;
-        sum_x_d[p.cluster] += p.x;
-        sum_y_d[p.cluster] += p.y;
-        sum_z_d[p.cluster] += p.z;
-
-        // reset distance
-        p.min_dist = max;
-        points_d[index] = p;
-    }
-}
-
-// Function that launches the CUDA kernel
-extern "C" void cuda_Kernel_1(Point* points_h, int points_size, Point c, int cluster_id) {
-    Point* points_d;
-    Point* c_d;
-    int new_points_size = sqrt(points_size); 
-
-
+extern "C" void cuda_setup(Point* points_h, Point** points_d, int points_size, Point** centroids_d, int k) {
     //Allocate device pointers and copy them to the device
-    checkCuda(cudaMalloc((void **) &points_d, sizeof(Point)*points_size));
-    checkCuda(cudaMalloc((void **) &c_d, sizeof(Point)));
+    checkCuda(cudaMalloc((void **) points_d, sizeof(Point)*points_size));
+    checkCuda(cudaDeviceSynchronize());
+    printf("allocating centroids_d\n");
+    checkCuda(cudaMalloc((void **) centroids_d, sizeof(Point)*k)); // just allocate the memory now, we will memcpy every kernel call
     checkCuda(cudaDeviceSynchronize());
 
-    checkCuda(cudaMemcpy(points_d, points_h, sizeof(Point)*points_size, cudaMemcpyHostToDevice));
-    checkCuda(cudaMemcpy(c_d, &c, sizeof(Point), cudaMemcpyHostToDevice));  
+    checkCuda(cudaMemcpy(*points_d, points_h, sizeof(Point)*points_size, cudaMemcpyHostToDevice));
     checkCuda(cudaDeviceSynchronize());
-    
-    dim3 DimGrid(ceil(new_points_size/32.0), ceil(new_points_size/32.0), 1);
-    dim3 DimBlock(32, 32, 1);
+}
 
-    //Launch kernel
-    my_Kernel_1<<<DimGrid, DimBlock>>>(points_d, points_size, c_d, cluster_id);
-    checkCuda(cudaDeviceSynchronize());
-
+extern "C" void cuda_cleanup(Point* points_h, Point* points_d, int points_size, Point* centroids_d) {
     //copy device points to host points
     checkCuda(cudaMemcpy(points_h, points_d, sizeof(Point)*points_size, cudaMemcpyDeviceToHost));
     checkCuda(cudaDeviceSynchronize());
     
     //Free device pointers
     checkCuda(cudaFree(points_d));
-    checkCuda(cudaFree(c_d));
+    checkCuda(cudaFree(centroids_d));
     checkCuda(cudaDeviceSynchronize());
 }
 
+// Function that launches the CUDA kernel
+extern "C" void cuda_distances_kernel(Point* points_d, int points_size, Point* centroids_h, Point* centroids_d, int k) {
+    // XXX: why is it 2d?
+    dim3 DimGrid(ceil(points_size/32.0));
+    dim3 DimBlock(32);
+
+    // copy the centroids over
+    //checkCuda(cudaMalloc((void **) &centroids_d, sizeof(Point)*k)); // just allocate the memory now, we will memcpy every kernel call
+    checkCuda(cudaMemcpy(centroids_d, centroids_h, sizeof(Point)*k, cudaMemcpyHostToDevice));
+    //checkCuda(cudaMemcpy(points_d, points_h, sizeof(Point)*points_size, cudaMemcpyHostToDevice));
+    checkCuda(cudaDeviceSynchronize());
+
+    //Launch kernel
+    distances_k<<<DimGrid, DimBlock>>>(points_d, points_size, centroids_d, k);
+    //checkCuda(cudaFree(centroids_d));
+    checkCuda(cudaDeviceSynchronize());
+}
+
+/*
 // Function that launches the CUDA kernel
 extern "C" void cuda_Kernel_2(Point* points_h, int points_size, int* n_points, double* sum_x, double* sum_y, double* sum_z, int k) {
     Point* points_d;
@@ -129,3 +128,5 @@ extern "C" void cuda_Kernel_2(Point* points_h, int points_size, int* n_points, d
     checkCuda(cudaFree(sum_z_d));
     checkCuda(cudaDeviceSynchronize());
 }
+
+*/
--- src/shared_gpu_k_clustering.c
+++ src/shared_gpu_k_clustering.c
@@ -1,50 +1,83 @@
 #include <float.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "shared_gpu_k_clustering.h"
 
-extern void cuda_Kernel_1(Point* points_h, int points_size, Point c, int cluster_id);
-extern void cuda_Kernel_2(Point* points_h, int points_size, int* n_points, double* sum_x, double* sum_y, double* sum_z, int k);
+extern void cuda_distances_kernel(Point* points_d, int points_size, Point* centroids_h, Point* centroids_d, int k);
+extern void cuda_setup(Point* points_h, Point** points_d, int points_size, Point** centroids_d, int k);
+extern void cuda_cleanup(Point* points_h, Point* points_d, int points_size, Point* centroids_d);
+//extern void cuda_Kernel_2(Point* points_h, int points_size, int* n_points, double* sum_x, double* sum_y, double* sum_z, int k);
 
 // perform the c clustering
-void k_means_clustering(Point* points, int points_size, int epochs, int k) {
-	Point centroids[k];
-	srand(42);
-	for (int i = 0; i < k; ++i) {
-		centroids[i] = points[rand() % points_size];
-	}
+void k_means_clustering(Point* points_h, int points_size, int epochs, int k) {
+    Point* points_d;
+    Point* centroids_d;
+    printf("cb4 %p\n", centroids_d);
+    cuda_setup(points_h, &points_d, points_size, &centroids_d, k);
+    printf("cb4 %p\n", centroids_d);
+    Point* centroids_h = malloc(sizeof(Point) * k);
 
-	for (int i = 0; i < epochs; ++i) {
-		// For each centroid, compute distance from centroid to each point
-		// and update point's cluster if necessary
-		for (int cluster_id = 0; cluster_id < k; ++cluster_id){
-			Point c = centroids[cluster_id];
-			cuda_Kernel_1(points, points_size, c, cluster_id);
-		}
+    srand(42);
+    for (int i = 0; i < k; ++i) {
+        centroids_h[i] = points_h[rand() % points_size];
+    }
 
-		// Create vectors to keep track of data needed to compute means
-		int n_points[k];
-		double sum_x[k];
-		double sum_y[k];
-		double sum_z[k];
-		for (int j = 0; j < k; ++j) {
-			n_points[j] = 0;
-			sum_x[j] = 0.0;
-			sum_y[j] = 0.0;
-			sum_z[j] = 0.0;
-		}
+    for (int i = 0; i < epochs; ++i) {
+        // For each centroid, compute distance from centroid to each point
+        // and update point's cluster if necessary
+
+        // we might need centroids to be in GPU memory...
+
+        /*
+          for (int cluster_id = 0; cluster_id < k; ++cluster_id){
+          Point c = centroids_h[cluster_id];
+          cuda_Kernel_1(points_h, points_size, c, cluster_id, points_d, c_d);
+          }
+        */
+        printf("calling kernel\n");
+        cuda_distances_kernel(points_d, points_size, centroids_h, centroids_d, k);
+        printf("done kernel\n");
+
+        // Create vectors to keep track of data needed to compute means
+        int n_points[k];
+        double sum_x[k];
+        double sum_y[k];
+        double sum_z[k];
+        for (int j = 0; j < k; ++j) {
+            n_points[j] = 0;
+            sum_x[j] = 0.0;
+            sum_y[j] = 0.0;
+            sum_z[j] = 0.0;
+        }
 
 
-    //Kernel2
-	cuda_Kernel_2(points, points_size, n_points, sum_x, sum_y, sum_z, k);
+        // XXX: for now, just do this in serial. the above loop is much more computation heavy anyways!
+        // XXX: this is going to fail, because we aren't copying points back to. This MUST also be
+        //  done in GPU so we can keep points over there
+        //Kernel2
+        //cuda_Kernel_2(points_h, points_size, n_points, sum_x, sum_y, sum_z, k);
+        for (int j = 0; j < points_size; j++) {
+            Point* p = &points_h[j];
+            n_points[p->cluster] += 1;
+            sum_x[p->cluster] += p->x;
+            sum_y[p->cluster] += p->y;
+            sum_z[p->cluster] += p->z;
 
-		for (int cluster_id = 0; cluster_id < k; cluster_id++) {
-			Point* c = &centroids[cluster_id];
-			if (n_points[cluster_id] != 0) {
-				c->x = sum_x[cluster_id] / n_points[cluster_id];
-				c->y = sum_y[cluster_id] / n_points[cluster_id];
-				c->z = sum_z[cluster_id] / n_points[cluster_id];
-			}
-		}
-	}
+            // reset distance
+            p->min_dist = DBL_MAX;
+        }
+
+        for (int cluster_id = 0; cluster_id < k; cluster_id++) {
+            Point* c = &centroids_h[cluster_id];
+            if (n_points[cluster_id] != 0) {
+                c->x = sum_x[cluster_id] / n_points[cluster_id];
+                c->y = sum_y[cluster_id] / n_points[cluster_id];
+                c->z = sum_z[cluster_id] / n_points[cluster_id];
+            }
+        }
+    }
+
+    free(centroids_h);
+    cuda_cleanup(points_h, points_d, points_size, centroids_d);
 }
