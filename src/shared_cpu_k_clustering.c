df8061cf785c8db9da79399cc2890fea839c13bd
Samuel Johnson <97634659+JohnsonSamuel55@users.noreply.github.com>
2023-12-04 16:35:29 -0700

    Fixed race condition and updated shared CPU implementation

--- src/shared_cpu_k_clustering.c
+++ src/shared_cpu_k_clustering.c
@@ -6,23 +6,31 @@
 #include "shared_cpu_k_clustering.h"
 #include "constants.h"
 
-// perform the c clustering
+// perform the k means clustering clustering
 void k_means_clustering(Point* points, int points_size) {
 	Point centroids[K_CLUSTERS];
-	//srand(time(0));
 	srand(42);
-  int thread_count = 4;
+  int thread_count = 4;		
+	
+// Create vectors to keep track of data needed to compute means
+	int n_points[K_CLUSTERS];
+	double sum_x[K_CLUSTERS];
+	double sum_y[K_CLUSTERS];
+	double sum_z[K_CLUSTERS];
+
+	// Create initial centroids
 	for (int i = 0; i < K_CLUSTERS; ++i) {
 		centroids[i] = points[rand() % points_size];
 	}
 
+	#pragma omp parallel num_threads(thread_count) shared(sum_x, sum_y, sum_z, n_points)	
 	for (int i = 0; i < N_EPOCHS; ++i) {
 		// For each centroid, compute distance from centroid to each point
 		// and update point's cluster if necessary
 		for (int cluster_id = 0; cluster_id < K_CLUSTERS; ++cluster_id){
 			Point c = centroids[cluster_id];
 
-      #pragma omp parallel for num_threads(thread_count)
+      #pragma omp for
 			for (int j = 0; j < points_size; ++j) {
 				Point* p = &points[j];
 				double dist = point_distance(c, *p);
@@ -33,11 +41,7 @@ void k_means_clustering(Point* points, int points_size) {
 			}
 		}
 
-		// Create vectors to keep track of data needed to compute means
-		int n_points[K_CLUSTERS];
-		double sum_x[K_CLUSTERS];
-		double sum_y[K_CLUSTERS];
-		double sum_z[K_CLUSTERS];
+		#pragma omp for
 		for (int j = 0; j < K_CLUSTERS; ++j) {
 			n_points[j] = 0;
 			sum_x[j] = 0.0;
@@ -46,7 +50,7 @@ void k_means_clustering(Point* points, int points_size) {
 		}
 
 		// Iterate over points to append data to centroids
-    #pragma omp parallel for num_threads(thread_count)
+   #pragma omp for reduction(+:n_points) reduction(+:sum_x) reduction(+:sum_y) reduction(+:sum_z)
 		for (int j = 0; j < points_size; j++) {
 			Point* p = &points[j];
 			n_points[p->cluster] += 1;
@@ -58,6 +62,8 @@ void k_means_clustering(Point* points, int points_size) {
 			p->min_dist = DBL_MAX;
 		}
 
+		// Calculate new centroids
+		#pragma omp for
 		for (int cluster_id = 0; cluster_id < K_CLUSTERS; cluster_id++) {
 			Point* c = &centroids[cluster_id];
 			if (n_points[cluster_id] != 0) {
@@ -68,3 +74,4 @@ void k_means_clustering(Point* points, int points_size) {
 		}
 	}
 }
+
